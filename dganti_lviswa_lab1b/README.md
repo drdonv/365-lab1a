8ba98e2e956a853568b7af0d1c413c24137f0b31
drdonv <realdrdonv@gmail.com>
2024-10-04 21:30:35 -0700

    lab 1-b done s/o lviswa hella

---
+++ dganti_lviswa_lab1b/README.md
@@ -0,0 +1,18 @@
+Team: Dhanvi Ganti, Lakshana Viswa
+
+Compile this program by running the command "python schoolsearch.py" in your terminal. Then, use the commands below to access the data you would like. The brackets indicate optional parameters.
+
+• S[tudent]: <lastname> [B[us]] # Find data on a student by last name. Include the optional bus parameter to get bus info
+• T[eacher]: <lastname> # Retrieve names of students teacher teaches
+• B[us]: <number> # Display info about a certain bus number
+• G[rade]: <number> [H[igh]|L[ow]] # Display info about a grade and optionally the highest/lowest of the grade
+• A[verage]: <number> # Average GPA info of a given grade
+• I[nfo]
+• Q[uit]
+• C[lassroom]: <number> # Returns students of a classroom
+• FindTeacher: <number> # Returns the name of a teacher assigned to a given classroom number
+• GradeTeachers: <number> # Returns all teachers teaching a certain grade
+• E[nrollments] # Enrollment info of each classroom
+• Average:/Avg: <number> # Returns average GPA of a student in a certain grade
+• A[nalytics]: <"GradeGPA" / "TeacherGPA" / "BusGPA"> # Find the average GPA of students, GPA by teacher, or GPA by bus
+• FindTeacher: <number> # Find the teacher for a given classroom number
--- dganti_lviswa_lab1b/schoolsearch.py
+++ dganti_lviswa_lab1b/schoolsearch.py
@@ -1,5 +1,5 @@
-# Team: Dhanvi Ganti, Lakshana Viswa
 import pandas as pd
+import shlex
 
 list_cols = ["last_name", "first_name", "grade", "classroom", "bus", "gpa"]
 teacher_cols = ["teacher_last", "teacher_first", "classroom"]
@@ -8,122 +8,221 @@ filepath = "list.txt"
 filepath2 = "teachers.txt"
 
 try:
-    df = pd.read_csv(filepath, names=list_cols, header=None)
-    df2 = pd.read_csv(filepath2, names=teacher_cols, header=None)
+    df_students = pd.read_csv(filepath, names=list_cols, header=None)
+    df_teachers = pd.read_csv(filepath2, names=teacher_cols, header=None)
 
-    for index, row in df2.iterrows():
+    for index, row in df_teachers.iterrows():
         teacher_first = row["teacher_first"]
-        #print(teacher_first)
         teacher_last = row["teacher_last"]
         classroom = row["classroom"]
 
-        df.loc[df['classroom'] == classroom, 'teacher_last'] = teacher_last
-        df.loc[df['classroom'] == classroom, 'teacher_first'] = teacher_first
+        df_students.loc[df_students['classroom'] == classroom, 'teacher_last'] = teacher_last
+        df_students.loc[df_students['classroom'] == classroom, 'teacher_first'] = teacher_first
 
-    df["grade"] = df["grade"].astype(int)
-    df["classroom"] = df["classroom"].astype(int)
-    df["bus"] = df["bus"].astype(int)
-    df["gpa"] = df["gpa"].astype(float)
+    try:
+        df_students["grade"] = df_students["grade"].astype(int)
+        df_students["classroom"] = df_students["classroom"].astype(int)
+        df_students["bus"] = df_students["bus"].astype(int)
+        df_students["gpa"] = df_students["gpa"].astype(float)
+    except ValueError:
+        print("Error converting some columns to the appropriate data types. Check your data.")
 
     def callSchoolSearch():
-        schoolSearch(input("Please input your command:\n"))
+        while True:
+            command = input("Please input your command:\n")
+            if command.lower() == "q":
+                break
+            schoolSearch(command)
 
-    def schoolSearch(input):
-        firstLetter = input[0]
-        inputArr = input.split()
-        printBusRoute = False
+    def schoolSearch(command):
+        inputArr = shlex.split(command)
 
-        if (firstLetter == "S" or inputArr[0] == "Student:"):
-            # error checks to ensure wrong num of args not passed?
-            if (len(inputArr) == 3):
-                # Re initiates call
-                if (inputArr[2][0] != "B" and inputArr[2] != "Bus"):
-                    callSchoolSearch()
-                printBusRoute = True
+        if inputArr[0] in ["S:", "Student:"]:
+            search_student(inputArr)
+        elif inputArr[0] in ["T:", "Teacher:"]:
+            search_teacher(inputArr)
+        elif inputArr[0] in ["G:", "Grade:"]:
+            if len(inputArr) > 2 and inputArr[2] == "H":  # Handle 'high GPA' in grade
+                search_grade_high_gpa(inputArr)
+            elif len(inputArr) > 2 and inputArr[2] == "L":  # Handle 'low GPA' in grade
+                search_grade_low_gpa(inputArr) 
+            elif len(inputArr) > 2 and inputArr[2] == "T":  # Handle 'teachers teaching grade'
+                search_grade_teachers(inputArr)
+            else:
+                search_grade(inputArr)
+        elif inputArr[0] in ["B:", "Bus:"]:
+            search_bus(inputArr)
+        elif inputArr[0] in ["C:", "Classroom:"]:
+            search_classroom_students(inputArr)
+        elif inputArr[0] == "FindTeacher:":
+            search_classroom_teachers(inputArr)
+        elif inputArr[0] == "GradeTeachers:":
+            search_grade_teachers(inputArr)
+        elif inputArr[0] in ["E", "Enrollments"]:
+            report_enrollments()
+        elif inputArr[0] in ["Average:", "Avg:"]:
+            search_average(inputArr)
+        elif inputArr[0] in ["A:", "Analytics:"]:
+            run_analytics(inputArr)
+        elif inputArr[0] in ["I", "Info"]:
+            displayInfo()
+        elif inputArr[0].lower() in ["q", "Q"]:
+            print("Exiting program.")
+            exit()
+        else:
+            print("Invalid command, please try again.")
 
-            for index in df.index:
-                lastName = df.loc[index, "last_name"]
-                if (lastName == inputArr[1]):
-                    if (printBusRoute == False):
-                        print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"] + ", " + str(df.loc[index, "grade"]) + ", " + str(df.loc[index, "classroom"]) + ", " + df.loc[index, "teacher_last"] + ", " + df.loc[index, "teacher_first"])
-                    else:
-                        print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"] + ", " + str(df.loc[index, "bus"]))
-                
-        if (firstLetter == "T" or inputArr[0] == "Teacher:"):
-            if (len(inputArr) == 2):
-                for index in df.index:
-                    teacherLast = df.loc[index, "teacher_last"]
-                    if (teacherLast == inputArr[1]):
-                        print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"])
-        
-        if (firstLetter == "G" or inputArr[0] == "Grade:"):
-            if (len(inputArr) == 2):
-                for index in df.index:
-                    grade = df.loc[index, "grade"]
-                    if (grade.astype(str) == inputArr[1]):
-                        print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"])
-            elif (len(inputArr) == 3):
-                if (inputArr[2][0] == "T"):
-                    grade = int(inputArr[1])
-                    dupeSet = set()
+    def search_student(inputArr):
+        printBusRoute = len(inputArr) == 3 and (inputArr[2] in ["Bus", "B"])
+        last_name = inputArr[1]
+        students = df_students[df_students['last_name'] == last_name]
 
-                    for index in df.index:
-                        if df.loc[index, 'grade'] ==  grade:
-                            teacher_last = df.loc[index, 'teacher_last']
-                            teacher_first = df.loc[index, 'teacher_first']
-                            if (teacher_last not in dupeSet):
-                                print(teacher_last + ", " + teacher_first)
-                                dupeSet.add(teacher_last)
+        for i, student in students.iterrows():
+            if printBusRoute:
+                print(f"{student['last_name']}, {student['first_name']}, Bus: {student['bus']}")
+            else:
+                print(f"{student['last_name']}, {student['first_name']}, Grade: {student['grade']}, "
+                      f"Classroom: {student['classroom']}, Teacher: {student['teacher_last']}, {student['teacher_first']}")
 
-                else:
-                    searchHighest = True
+    def search_teacher(inputArr):
+        teacher_last = inputArr[1]
+        classrooms = df_teachers[df_teachers['teacher_last'] == teacher_last]['classroom']
+        for classroom in classrooms:
+            students = df_students[df_students['classroom'] == classroom]
+            for i, student in students.iterrows():
+                print(f"{student['last_name']}, {student['first_name']}")
 
-                    if (inputArr[2][0] == "L" or inputArr[2] == "Low"):
-                        searchHighest = False
-                    
-                    grade = int(inputArr[1])
-                    extremeGpa = 0
+    def search_grade(inputArr):
+        grade = int(inputArr[1])
+        students = df_students[df_students['grade'] == grade]
+        for i, student in students.iterrows():
+            print(f"{student['last_name']}, {student['first_name']}")
 
-                    if (searchHighest):
-                        extremeGpa = df[df['grade'] == grade]['gpa'].max()
-                    else:
-                        extremeGpa = df[df['grade'] == grade]['gpa'].min()
-
-                    for index in df.index:
-                        grd = df.loc[index, "grade"]
-                        gpa = df.loc[index, "gpa"]
-                        if (searchHighest):
-                            if (grd == grade and gpa >= extremeGpa):
-                                print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"] + ", " + str(df.loc[index, "gpa"]) + ", " + df.loc[index, "teacher_last"] + ", " + df.loc[index, "teacher_first"] + ", " + str(df.loc[index, "classroom"]))
-                        else:
-                            if (grd == grade and gpa <= extremeGpa):
-                                print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"] + ", " + str(df.loc[index, "gpa"]) + ", " + df.loc[index, "teacher_last"] + ", " + df.loc[index, "teacher_first"] + ", " + str(df.loc[index, "classroom"]))
-
-        if (firstLetter == "B" or inputArr[0] == "Bus:"):
-            if (len(inputArr) == 2):
-                for index in df.index:
-                    bus = df.loc[index, "bus"]
-                    if (bus.astype(str) == inputArr[1]):
-                        print(df.loc[index, "last_name"] + ", " + df.loc[index, "first_name"] + ", " + str(df.loc[index, "grade"]) + ", " + str(df.loc[index, "classroom"]))
-        
-        if (firstLetter == "A" or inputArr[0] == "Average:"):
-            if (len(inputArr) == 2):
-                grade = int(inputArr[1])
-                averageGpa = round(df[df['grade'] == grade]['gpa'].mean(), 2)
-                print(str(grade) + ", " + str(averageGpa))
-
-        if (firstLetter == "I"):
-            for grade in range(0, 7):
-                print(str(grade) + ": " + str((df['grade'] == grade).sum()))
-                                                                
-        if (firstLetter == "Q"):
+    def search_bus(inputArr):
+        if len(inputArr) < 2:
+            print("Error: Please provide a bus number.")
             return
-        
-        callSchoolSearch()
+        try:
+            bus = int(inputArr[1])
+        except ValueError:
+            print("Error: Bus number should be a valid number.")
+            return
+        students = df_students[df_students['bus'] == bus]
+        if students.empty:
+            print(f"No students found for bus route '{bus}'.")
+            return
+        for i, student in students.iterrows():
+            print(f"{student['last_name']}, {student['first_name']}, Grade: {student['grade']}, Classroom: {student['classroom']}")
+
+    def search_classroom_students(inputArr):
+        if len(inputArr) < 2:
+            print("Error: Please provide a classroom number.")
+            return
+        classroom = inputArr[1].strip()
+        try:
+            classroom_int = int(classroom)
+        except ValueError:
+            print("Error: Classroom number should be a valid number.")
+            return
+        students = df_students[df_students['classroom'] == classroom_int] 
+        if students.empty:
+            print(f"No students found for classroom '{classroom_int}'.")
+            return
+        for i, student in students.iterrows():
+            print(f"{student['last_name']}, {student['first_name']}")
+
+    def search_classroom_teachers(inputArr):
+        if len(inputArr) < 2:
+            print("Error: Please provide a classroom number.")
+            return
+        classroom = inputArr[1].strip()
+        try:
+            classroom_int = int(classroom) 
+        except ValueError:
+            print("Error: Classroom number should be a valid number.")
+            return
+        teachers = df_teachers[df_teachers['classroom'] == classroom_int]
+        if not teachers.empty:
+            for i, teacher in teachers.iterrows():
+                print(f"Teacher: {teacher['teacher_last']}, {teacher['teacher_first']}")
+        else:
+            print(f"No teachers found for classroom {classroom_int}")
+
+    def search_grade_teachers(inputArr):
+        grade = int(inputArr[1])
+        classrooms = df_students[df_students['grade'] == grade]['classroom'].unique()
+        teachers = df_teachers[df_teachers['classroom'].isin(classrooms)]
+        for i, teacher in teachers.iterrows():
+            print(f"Teacher: {teacher['teacher_last']}, {teacher['teacher_first']}")
+
+    def report_enrollments():
+        enrollments = df_students.groupby('classroom').size().reset_index(name='enrollment')
+        enrollments = enrollments.sort_values(by='classroom')
+        for i, row in enrollments.iterrows():
+            print(f"Classroom: {row['classroom']}, Students: {row['enrollment']}")
+
+    def run_analytics(inputArr):
+        if inputArr[1] == "GradeGPA":
+            report_gpa_by_grade()
+        elif inputArr[1] == "TeacherGPA":
+            report_gpa_by_teacher()
+        elif inputArr[1] == "BusGPA":
+            report_gpa_by_bus()
+
+    def report_gpa_by_grade():
+        gpa_by_grade = df_students.groupby('grade')['gpa'].mean().reset_index()
+        for i, row in gpa_by_grade.iterrows():
+            print(f"Grade: {row['grade']}, Average GPA: {round(row['gpa'], 2)}")
+
+    def report_gpa_by_teacher():
+        df_students['classroom'] = df_students['classroom'].astype(str).str.strip()
+        df_teachers['classroom'] = df_teachers['classroom'].astype(str).str.strip()
+        merged_df = pd.merge(df_students, df_teachers, on='classroom', how='left')
+        merged_df.rename(columns={'teacher_last_y': 'teacher_last', 'teacher_first_y': 'teacher_first'}, inplace=True)
+        gpa_by_teacher = merged_df.groupby(['teacher_last', 'teacher_first'])['gpa'].mean().reset_index()
+        if gpa_by_teacher.empty:
+            print("No data available for GPA by teacher.")
+        else:
+            for i, row in gpa_by_teacher.iterrows():
+                print(f"Teacher: {row['teacher_last']} {row['teacher_first']}, Average GPA: {round(row['gpa'], 2)}")
+
+    def report_gpa_by_bus():
+        gpa_by_bus = df_students.groupby('bus')['gpa'].mean().reset_index()
+        for i, row in gpa_by_bus.iterrows():
+            print(f"Bus: {row['bus']}, Average GPA: {round(row['gpa'], 2)}")
+
+    def search_grade_high_gpa(inputArr):
+        grade = int(inputArr[1])
+        students = df_students[df_students['grade'] == grade]
+        if not students.empty:
+            highest_gpa_student = students.loc[students['gpa'].idxmax()]
+            print(f"{highest_gpa_student['last_name']}, {highest_gpa_student['first_name']}, "
+                  f"{round(highest_gpa_student['gpa'], 2)}, {highest_gpa_student['teacher_last']}, {highest_gpa_student['classroom']}")
+        else:
+            print(f"No students found for grade {grade}.")
+
+    def search_grade_low_gpa(inputArr):
+        grade = int(inputArr[1])
+        students = df_students[df_students['grade'] == grade]
+        if not students.empty:
+            lowest_gpa_student = students.loc[students['gpa'].idxmin()]
+            print(f"{lowest_gpa_student['last_name']}, {lowest_gpa_student['first_name']}, "
+                  f"{round(lowest_gpa_student['gpa'], 2)}, {lowest_gpa_student['teacher_last']}, {lowest_gpa_student['classroom']}")
+        else:
+            print(f"No students found for grade {grade}.")
+
+    def search_average(inputArr):
+        grade = int(inputArr[1])
+        avg_gpa = df_students[df_students['grade'] == grade]['gpa'].mean()
+        print(f"Grade: {grade}, Average GPA: {round(avg_gpa, 2)}")
+
+    def displayInfo():
+        for grade in range(0, 7):
+            print(str(grade) + ": " + str((df_students['grade'] == grade).sum()))
 
     callSchoolSearch()
+
 except FileNotFoundError:
     print("Invalid file, check your filepath.")
-
 except pd.errors.ParserError:
     print("Bad CSV formatting")
